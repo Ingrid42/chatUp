e1b63aed2bf2838bd23310cf8884a5487a44cabf
Martin Florian <florian.martin@insa-rouen.fr>
2017-03-15 12:50:22 +0100

    Ajout automatique des utilisateurs à la discussion à sa création + Correction des warning

--- serveur/compile.sh
+++ serveur/compile.sh
@@ -1,3 +1,3 @@
-javac -d classes -cp classes ./messagerie/serveur/discussion/*.java
-javac -d classes -cp classes ./messagerie/serveur/utilisateur/*.java
-javac -d classes -cp classes ./messagerie/serveur/filtre/*.java
+javac -Xlint -d classes -cp classes ./messagerie/serveur/discussion/*.java
+javac -Xlint -d classes -cp classes ./messagerie/serveur/utilisateur/*.java
+javac -Xlint -d classes -cp classes ./messagerie/serveur/filtre/*.java
