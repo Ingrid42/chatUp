462865b1b1a1cda2975b32402c0f3a146fd9de67
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-03-15 10:58:27 +0100

    maj serveur express pour dependances jquery

--- client/express.js
+++ client/express.js
@@ -6,6 +6,7 @@ var port = 3000;
 app.use("/", express.static('src'));
 app.use("/bootstrap/css", express.static('node_modules/bootstrap/dist/css'));
 app.use("/bootstrap/js", express.static('node_modules/bootstrap/dist/js'));
+app.use("/jquery", express.static('node_modules/jquery/dist/'));
 
 app.listen(port, () => {
   console.log('Server listening on port ' + port + '!')
