05798d46fcc5f7fcbf28a2b715ec79be5a1a921c
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-03-15 10:52:37 +0100

    update serveur express pour dependances bootstrap

--- client/express.js
+++ client/express.js
@@ -4,6 +4,8 @@ var opener = require('opener');
 var port = 3000;
 
 app.use("/", express.static('src'));
+app.use("/bootstrap/css", express.static('node_modules/bootstrap/dist/css'));
+app.use("/bootstrap/js", express.static('node_modules/bootstrap/dist/js'));
 
 app.listen(port, () => {
   console.log('Server listening on port ' + port + '!')
