71377d364a074ab6b1d1107ffce0f4eac469d6c0
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-03-22 09:35:12 +0100

    add socket.io

---
+++ client/src/js/index.js
@@ -0,0 +1,9 @@
+const connexion = function() {
+  console.log("Init socket.io");
+  var socket = io('http://localhost:4000');
+  socket.on('connect', function(){});
+  socket.on('event', function(data){});
+  socket.on('disconnect', function(){});
+}
+
+//$(document).ready(connexion);
