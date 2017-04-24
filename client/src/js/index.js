79d3e7e0f16ff8d98a35d669066bc80c0e9a0f60
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-04-24 18:23:16 +0200

    debut de navigation au sein de l'application web

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -11,11 +11,34 @@ const init = function() {
 }
 
 const onEvent = function() {
-  $('image-input').on('click', function() {
+  $('#image-input').on('click', function() {
 
   });
 
-  $();
+  $('#switchToConnexion').on('click', function() {
+    $('#pageConnexion').addClass('hidden');
+    $('#navbar').addClass('hidden');
+    $('#pageMessagerie').removeClass('hidden');
+    $('#navbarMessagerie').removeClass('hidden');
+  });
+
+  $('#switchToInscription').on('click', function() {
+    $('#pageConnexion').addClass('hidden');
+    $('#pageInscription').removeClass('hidden');
+  });
+
+  $('.switchToParameters').on('click', function() {
+    $('#pageMessagerie').addClass('hidden');
+    $('#navbarMessagerie').addClass('hidden')
+    $('#pageParametres').removeClass('hidden');
+  });
+
+  $('.switchToContacts').on('click', function() {
+    $('#pageMessagerie').addClass('hidden');
+    $('#navbarMessagerie').addClass('hidden')
+    $('#pageContacts').removeClass('hidden');
+    $('#navbarContacts').removeClass('hidden');
+  });
 }
 
 $(document).ready(init)
