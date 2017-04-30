d1ab4554af72e8088c14d2afbd0a82fca65be321
Florian Martin <Florian Martin>
2017-04-30 13:30:50 +0200

    Documentation des classes associées aux discussions

--- serveur/messagerie/serveur/discussion/Discussion.java
+++ serveur/messagerie/serveur/discussion/Discussion.java
@@ -8,13 +8,32 @@ import messagerie.serveur.exception.*;
 
 import java.io.Serializable;
 
+/**
+ * Classe représentant une discussion entre utilisateurs.
+ */
 public class Discussion implements Serializable {
     public final static long serialVersionUID = 531L;
 
+    /**
+     * Indice autoincrémenté afin d'identifier chaque nouvelle discussion.
+     */
     private static int idIncr = 0;
+
+    /**
+     * Identifiant de la discussion.
+     */
     private int id;
+
+    /**
+     * Liste des utilisateurs prennant part à la discussion.
+     */
     private List<Utilisateur> utilisateurs ;
 
+    /**
+     * Création d'une discussion.
+     * @param utilisateurs Liste des utilisateurs prennant part à la discussion.
+     * @throws DiscussionException Se déclenche si il y a moins de 2 utilisateurs prennant part à la discussion.
+     */ 
     public Discussion(List<Utilisateur> utilisateurs) throws DiscussionException {
         if (utilisateurs.size() < 2)
             throw new DiscussionException("Une discussion doit avoir au minimum 2 utilisateurs.");
@@ -26,9 +45,17 @@ public class Discussion implements Serializable {
 			u.addDiscussion(this);
     }
 
+    /**
+     * Récupérer la liste des utilisateurs présents dans la discussion.
+     * @return Liste des utilisateurs prennant part à la discussion.
+     */
     public List<Utilisateur> getUtilisateurs(){
         return this.utilisateurs ;
     }
 
+    /**
+     * Récupérer l'identifiant de la discussion.
+     * @return Identifiant de la discussion.
+     */
     public long getId() { return this.id; }
 }
--- serveur/messagerie/serveur/discussion/Message.java
+++ serveur/messagerie/serveur/discussion/Message.java
@@ -3,24 +3,60 @@ import java.io.Serializable;
 
 import messagerie.serveur.utilisateur.Utilisateur;
 
+/**
+ * Classe représentant un message.
+ */
 public class Message implements Serializable {
     public final static long serialVersionUID = 1257L;
-    private int id_discussion ;
+
+    /**
+     * Identifiant de la discussion à laquelle ce message se rattache.
+     */
+    private int idDiscussion ;
+
+    /**
+     * Contenu du message.
+     */
     private String message;
+
+    /**
+     * Utilisateur ayant envoyé le message.
+     */
     private Utilisateur utilisateur;
 
-    public Message(Utilisateur utilisateur, String message, int id_discussion){
+    /**
+     * Créer un message.
+     * @param utilisateur Utilisateur ayant écrit le message.
+     * @param message Contenu du message envoyé.
+     * @param idDiscussion Identifiant de la discussion à laquelle le message se rattache.
+     */
+    public Message(Utilisateur utilisateur, String message, int idDiscussion){
         this.message = message ;
         this.utilisateur = utilisateur ;
-        this.id_discussion = id_discussion ;
+        this.idDiscussion = idDiscussion ;
     }
+
+    /**
+     * Récupérer le contenu du message.
+     * @return Contenu du message.
+     */
     public String getMessage(){
       return this.message;
     }
+
+    /**
+     * Récupérer l'auteur du message.
+     * @return Utilisateur ayant écrit le message.
+     */
     public Utilisateur getUtilisateur(){
       return this.utilisateur ;
     }
+
+    /**
+     * Récupérer l'identifiant de la discussion à laquelle se rattache ce message.
+     * @return Identifiant d'une' discussion.
+     */
     public int getId(){
-      return this.id_discussion ;
+      return this.idDiscussion ;
     }
 }
