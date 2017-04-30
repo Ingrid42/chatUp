96d095c38e7cceb3db42b150f5bc5a08e9059112
Florian Martin <Florian Martin>
2017-04-30 13:15:18 +0200

    Documentation des exceptions

--- serveur/messagerie/serveur/exceptions/DiscussionException.java
+++ serveur/messagerie/serveur/exceptions/DiscussionException.java
@@ -1,16 +1,31 @@
 package messagerie.serveur.exception;
 
+/**
+ * Exceptions liées aux discussions.
+ */
 public class DiscussionException extends ServeurException {
 	public final static long serialVersionUID = 8935L;
 	
+	/**
+	 * Création de l'exception
+	 */
 	public DiscussionException() {
 		super();
 	}
 
+	/**
+	 * Création de l'exception
+	 * @param message Message lié à l'exception.
+	 */
 	public DiscussionException(String message) {
 		super(message);
 	}
 
+	/**
+	 * Création de l'exception
+	 * @param message Message lié à l'exception.
+	 * @param exception Exception à chainer.
+	 */
 	public DiscussionException(String message, Exception exception) {
 		super(message, exception);
 	}
--- serveur/messagerie/serveur/exceptions/ServeurException.java
+++ serveur/messagerie/serveur/exceptions/ServeurException.java
@@ -1,16 +1,32 @@
 package messagerie.serveur.exception;
 
+/**
+ * Exceptions liées au serveur. Cette classe ne peut être instanciée.
+ * Il est donc nécessaire de créer des classes héritées pour tous types d'exceptions liés au serveur.
+ */
 public abstract class ServeurException extends Exception {
 	public final static long serialVersionUID = 35421L;
 
+	/**
+	 * Création de l'exception
+	 */
 	public ServeurException() {
 		super();
 	}
 
+	/**
+	 * Création de l'exception
+	 * @param message Message lié à l'exception.
+	 */
 	public ServeurException(String message) {
 		super(message);
 	}
 
+	/**
+	 * Création de l'exception
+	 * @param message Message lié à l'exception.
+	 * @param exception Exception à chainer.
+	 */
 	public ServeurException(String message, Exception exception) {
 		super(message, exception);
 	}
