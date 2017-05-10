2d9fa2381d413095e9d780dd59d705e66da6db1b
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-05-10 10:20:15 +0200

    Réajout appel fonction qui fait tout fonctionner et que ingrid avait gentillement suprimé ! xD

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -10,47 +10,48 @@ const init = function() {
   navigateur.listen(session);
 };
 
-const inscriptionUtilisateur = function(session) {
+// const inscriptionUtilisateur = function(session) {
+//
+//   $('#inscriptionUtilisateur').on('click', function() {
+//     verification();
+//     let pseudonyme = $('#inputInscriptionPseudo').val();
+//     let prenom = $('#inputInscriptionPrenom').val();
+//     let mot_de_passe_confirmation = $('#inputInscriptionPasswordConfirmation').val();
+//
+//     let nom = $('#inputInscriptionNom').val();
+//     let mot_de_passe = $('#inputInscriptionPassword').val();
+//     let adresse_mel = $('#inputInscriptionEmail').val();
+//     let date_naissance = $('#inputInscriptionDatePicker').val();
+//
+//     let message = {
+//       contenu : {
+//       action : "creer_utilisateur",
+//         pseudonyme,
+//         nom,
+//         mot_de_passe,
+//         prenom,
+//         adresse_mel,
+//         date_naissance
+//       }
+//     };
+//     switchToConnexion();
+//     session.send(message);
+//   });
+// };
+//
+// const verification = function() {
+//
+//   var emailBox = document.getElementById("inputInscriptionEmail");
+//   var pseudo = document.getElementById("inputInscriptionPseudo");
+//   var error = document.getElementById("msgError");
+//   if(emailBox.value=="") {
+//     if(!error.hasChildNodes()) {
+//       emailBox.style.outline="solid Red";
+//     }
+//       error.appendChild(document.createTextNode("Champ obligatoire"));
+//   } else {
+//     document.forms['formInscription'].submit();
+//   }
+// };
 
-  $('#inscriptionUtilisateur').on('click', function() {
-    verification();
-    let pseudonyme = $('#inputInscriptionPseudo').val();
-    let prenom = $('#inputInscriptionPrenom').val();
-    let mot_de_passe_confirmation = $('#inputInscriptionPasswordConfirmation').val();
-
-    let nom = $('#inputInscriptionNom').val();
-    let mot_de_passe = $('#inputInscriptionPassword').val();
-    let adresse_mel = $('#inputInscriptionEmail').val();
-    let date_naissance = $('#inputInscriptionDatePicker').val();
-
-    let message = {
-      contenu : {
-      action : "creer_utilisateur",
-        pseudonyme,
-        nom,
-        mot_de_passe,
-        prenom,
-        adresse_mel,
-        date_naissance
-      }
-    };
-    switchToConnexion();
-    session.send(message);
-  });
-
-};
-const verification = function() {
-
-  var emailBox = document.getElementById("inputInscriptionEmail");
-  var pseudo = document.getElementById("inputInscriptionPseudo");
-  var error = document.getElementById("msgError");
-  if(emailBox.value=="") {
-    if(!error.hasChildNodes()) {
-      emailBox.style.outline="solid Red";
-    }
-      error.appendChild(document.createTextNode("Champ obligatoire"));
-  } else {
-    document.forms['formInscription'].submit();
-  }
-};
-
+$(document).ready(init());
